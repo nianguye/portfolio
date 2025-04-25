0da5123b763a518cebd4907c237f3d6f69389906
nianguye <nianguye2002@gmail.com>
2025-04-25 05:14:54 -0700

    added home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="flex min-h-screen flex-col bg-white">
           <Header />
-          <main className="flex-1">{children}</main>{" "}
+          <main className="flex-1 pb-16">{children}</main>{" "}
      
         </div>
         <footer>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,9 @@ export default function Header() {
     <header className="sticky top-0 z-10 bg-[#90D5FF]/95 border border-transparent shadow-none ">
       <div className="relative flex h-16 items-center justify-end mx-auto px-8">
         <nav className="flex gap-6">
+        <Link href="/" className="text-sm font-medium hover:underline">
+            Home
+          </Link>
           <Link href="#about" className="text-sm font-medium hover:underline">
             About
           </Link>
